31bafc7a8bf2783ef9ca619bfce09e3500a10b5f
Andrey Voronkov <voronkovaa@gmail.com>
2010-02-04 11:48:36 +0900

    Moving to divs

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -4,7 +4,14 @@ class PairsController < ApplicationController
   end
 
   def show_classrooms_schedule
-    
+    @days = [
+              'П о н е д е л ь н и к',
+              'В т о р н и к',
+              'С р е д а',
+              'Ч е т в е р г',
+              'П я т н и ц а',
+              'С у б б о т а'
+            ]
   end
 
   def show_groups_schedule
