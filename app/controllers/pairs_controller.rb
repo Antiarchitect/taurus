17bbca185b6a8d46a00982ddb3746c871eb0e1e8
Andrey Voronkov <voronkovaa@gmail.com>
2010-02-08 14:58:39 +0900

    Some fixes on a way to Ajax select

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -1,10 +1,5 @@
 class PairsController < ApplicationController
-  def index
-
-  end
-
-  def show_classrooms_schedule
-    @days = [
+  @@days = [
               'П о н е д е л ь н и к',
               'В т о р н и к',
               'С р е д а',
@@ -12,12 +7,19 @@ class PairsController < ApplicationController
               'П я т н и ц а',
               'С у б б о т а'
             ]
+  def index
+
+  end
+
+  def show_classrooms_schedule
+    @days = @@days
     @buildings = Building.all
     @classrooms = Classroom.all
   end
 
   def add_classroom
-    @classroom = Classroom.first(:conditions => {:id => params[:classroom]})
+    @days = @@days
+    @classroom = Classroom.first#(:conditions => {:id => params[:classroom]})
   end
 
   def show_groups_schedule
