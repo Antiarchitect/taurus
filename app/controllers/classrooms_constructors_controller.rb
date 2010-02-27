5cbbc3649c97b1d07dc8d0be5bb9538f37919aa8
Andrey Voronkov <voronkovaa@gmail.com>
2010-02-27 09:49:12 +0900

    Methods divided

--- app/controllers/classrooms_constructors_controller.rb
+++ app/controllers/classrooms_constructors_controller.rb
@@ -1,16 +1,17 @@
 class ClassroomsConstructorsController < ConstructorsController
+# RESTful methods
   def index
     
+  end
+
+  def show
+
   end
 
   def new
     @buildings = Building.all
     first_building_id = Building.first.id
     @classrooms = Classroom.all(:conditions => {:building_id => first_building_id})
-  end
-
-  def destroy
-    
   end
 
   def create
@@ -21,10 +22,19 @@ class ClassroomsConstructorsController < ConstructorsController
     @pairs = @classroom.pairs
   end
 
+  def edit
+
+  end
+  
   def update
+
+  end
+
+  def destroy
     
   end
 
+# Additional methods
   def update_classrooms_list
     @classrooms = Classroom.all(:conditions => {:building_id => params[:building]})
   end
