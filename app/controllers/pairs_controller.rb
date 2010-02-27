e109b5cbad19de165f4e8608da1e708ba51f4074
Andrey Voronkov <voronkovaa@gmail.com>
2010-02-27 10:24:46 +0900

    Fixes

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -1,31 +1,31 @@
 class PairsController < ApplicationController
   def update
     if params[:classroom]
-      Pair.update(params[:pair],
+      Pair.update(params[:id],
         :classroom_id => params[:classroom]
       )
     end
     if params[:lecturer]
-      Pair.update(params[:pair],
+      Pair.update(params[:id],
         :classroom_id => params[:lecturer]
       )
     end
     if params[:group]
-      Pair.update(params[:pair],
+      Pair.update(params[:id],
         :classroom_id => params[:lecturer]
       )
     end
     @days = ConstructorController.days
     @times = ConstructorController.times
     @weeks = ConstructorController.weeks
-    Timeslot.update(Pair.find(params[:pair]).timeslot.id,
+    Timeslot.update(Pair.find(params[:id]).timeslot.id,
       :week_number => params[:week],
       :week_day => params[:day],
       :pair_number => params[:time]
     )
     @classroom = Classroom.first(:conditions => {:id => params[:grid]})
     @pairs = @classroom.pairs
-    @pair = Pair.find(params[:pair])
+    @pair = Pair.find(params[:id])
     @container = params[:container]
   end
 end
