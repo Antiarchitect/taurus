997ace7f24677b50e2c2ba6745b97ca7b503bd3a
Andrey Voronkov <voronkovaa@gmail.com>
2010-02-27 14:20:05 +0900

    Some fixes

--- public/javascripts/constructor.js
+++ public/javascripts/constructor.js
@@ -6,7 +6,13 @@ function AddClassroomDrop(id, grid, week, day, time) {
       new Ajax.Request(
         '/pairs/'+pair.id, {
           method : 'put',
-          parameters : 'classroom=' + grid + '&week=' + week + '&day=' + day + '&time=' + time + '&container=' + receiver.id
+          parameters : {
+            classroom : grid,
+            week : week,
+            day : day,
+            time : time,
+            container : receiver.id
+          }
         }
       );
     }
