2ae4f328962ee21871bfb9e0daba310605f96b35
Andrey Voronkov <voronkovaa@gmail.com>
2010-03-09 09:20:40 +0900

    Corrected js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,20 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+function AddClassroomDrop(id, grid, week, day, time) {
+  Droppables.add(id, {
+    accept: 'pair',
+    hoverclass : 'hovered_receiver',
+    onDrop: function(pair, receiver) {
+      new Ajax.Request(
+        '/pairs/'+pair.id, {
+          method : 'put',
+          parameters : {
+            classroom : grid,
+            week : week,
+            day : day,
+            time : time,
+            container : receiver.id
+          }
+        }
+      );
+    }
+  });
+}
