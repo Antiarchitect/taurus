a1725e2f6cb86a61daa4d9579afa40dfe64579b8
Andrey Voronkov <andrey@linux-2n20.site>
2010-03-17 12:56:47 +0900

    changed from to_yaml to YAML.dump / YAML.load

--- app/controllers/classrooms_grids_controller.rb
+++ app/controllers/classrooms_grids_controller.rb
@@ -1,7 +1,7 @@
 class ClassroomsGridsController < GridsController
 # RESTful methods
   def index
-    cookies[:classrooms_grids] = [0].to_yaml
+    cookies[:classrooms_grids] = YAML.dump([0])
   end
 
   def show
@@ -11,7 +11,7 @@ class ClassroomsGridsController < GridsController
   def new
     @buildings = Building.all
     first_building_id = Building.first.id
-    grids = YAML::load(cookies[:classrooms_grids])
+    grids = YAML.load(cookies[:classrooms_grids])
     @classrooms = Classroom.all(:conditions => ["building_id = ? AND id NOT IN (?)", first_building_id, grids])
   end
 
@@ -21,9 +21,9 @@ class ClassroomsGridsController < GridsController
     @weeks = self.class.weeks
     @classroom = Classroom.find(params[:classrooms_grid][:classroom], :include => [{:pairs, :timeslot}])
     @pairs = @classroom.pairs
-    grids = YAML::load(cookies[:classrooms_grids])
+    grids = YAML.load(cookies[:classrooms_grids])
     grids << @classroom.id
-    cookies[:classrooms_grids] = grids.to_yaml
+    cookies[:classrooms_grids] = YAML.dump(grids)
   end
 
   def edit
@@ -36,14 +36,14 @@ class ClassroomsGridsController < GridsController
 
   def destroy
     @id = params[:id]
-    grids = YAML::load(cookies[:classrooms_grids])
+    grids = YAML.load(cookies[:classrooms_grids])
     grids.delete(@id.to_i)
-    cookies[:classrooms_grids] = grids.to_yaml
+    cookies[:classrooms_grids] = YAML.dump(grids)
   end
 
 # Additional methods
   def update_classrooms_list
-    grids = YAML::load(cookies[:classrooms_grids])
+    grids = YAML.load(cookies[:classrooms_grids])
     @classrooms = Classroom.all(:conditions => ["building_id = ? AND id NOT IN (?)", params[:building], grids])
   end
 end
