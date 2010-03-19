7e52c45bef12a98219f25c3dddc4bccaaabac18a
Andrey Voronkov <andrey@linux-2n20.site>
2010-03-19 14:33:14 +0900

    Still nothing

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -20,13 +20,12 @@
 //}
 
 function AddClassroomDrops(grid, weeks, days, times) {
-    for(week = 0; week < weeks; week++) {
-        for (day = 0; day < days; day++) {
-            for (time = 0; time < times; time++ ) {
+    for(week = 1; week <= weeks; week++) {
+        for (day = 1; day <= days; day++) {
+            for (time = 1; time <= times; time++ ) {
                 Droppables.add('container_grid'+ grid + '_week' + week + '_day' + day + '_time' + time, {
                     accept: 'pair',
                     hoverclass : 'hovered_receiver',
-                    name : name,
                     onDrop: function(pair, receiver) {
                         new Ajax.Request(
                           '/pairs/'+pair.id+'/update_on_drop', {
