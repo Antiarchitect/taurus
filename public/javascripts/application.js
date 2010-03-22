ab4bf68a81990f6604ad0fe0e080a6775eb33f93
Andrey Voronkov <andrey@linux-2n20.site>
2010-03-22 10:25:27 +0900

    1

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -20,29 +20,29 @@
 //}
 
     function AddClassroomDrops(grid, weeks, days, times) {
+        var drop = function(week, day, time) {
+            Droppables.add('container_grid'+ grid + '_week' + week + '_day' + day + '_time' + time, {
+                accept: 'pair',
+                hoverclass : 'hovered_receiver',
+                onDrop: function(pair, receiver) {
+                    new Ajax.Request(
+                        '/pairs/'+pair.id+'/update_on_drop', {
+                            method : 'put',
+                            parameters : {
+                                classroom : grid,
+                                week : week,
+                                day : day,
+                                time : time,
+                                container : receiver.id
+                            }
+                        }
+                    );
+                }
+            });
+        }
         for(week = 1; week <= weeks; week++) {
             for (day = 1; day <= days; day++) {
                 for (time = 1; time <= times; time++ ) {
-                    var drop = function(week, day, time) {
-                        Droppables.add('container_grid'+ grid + '_week' + week + '_day' + day + '_time' + time, {
-                            accept: 'pair',
-                            hoverclass : 'hovered_receiver',
-                            onDrop: function(pair, receiver) {
-                                new Ajax.Request(
-                                  '/pairs/'+pair.id+'/update_on_drop', {
-                                    method : 'put',
-                                    parameters : {
-                                      classroom : grid,
-                                      week : week,
-                                      day : day,
-                                      time : time,
-                                      container : receiver.id
-                                    }
-                                  }
-                                );
-                              }
-                        });
-                     }
                   drop(week, day, time);
                 }
             }
