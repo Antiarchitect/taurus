1cf11c218543c5b13bf3c512e6733b4e9d63848d
Andrey Voronkov <andrey@linux-2n20.site>
2010-03-29 11:46:22 +1000

    Nesting not work

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -2,5 +2,8 @@ class DepartmentsController < ApplicationController
   active_scaffold do |config|
     config.columns = [:name, :faculty]
     config.columns[:faculty].form_ui = :select
+    config.nested.add_link('Дисциплины', [:disciplines])
+    config.nested.add_link('Преподаватели', [:teaching_places])
+    config.nested.add_link('Специальности', [:specialities])
   end
 end
--- app/controllers/faculties_controller.rb
+++ app/controllers/faculties_controller.rb
@@ -1,5 +1,6 @@
 class FacultiesController < ApplicationController
   active_scaffold do |config|
     config.columns = [:name]
+    config.nested.add_link('Кафедры', [:departments])
   end
 end
