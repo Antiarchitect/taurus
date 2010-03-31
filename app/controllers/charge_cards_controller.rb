76640b14ac7177e89bf41837eb913fec2ef9e185
Andrey Voronkov <andrey@linux-2n20.site>
2010-03-31 16:31:52 +1000

    Some bells and whistles

--- app/controllers/charge_cards_controller.rb
+++ app/controllers/charge_cards_controller.rb
@@ -1,6 +1,8 @@
 class ChargeCardsController < ApplicationController
   active_scaffold do |config|
     config.columns = [:teaching_place, :jets, :discipline, :lesson_type]
+    config.list.columns << :groups
+    config.list.columns.exclude :jets
     config.columns[:discipline].form_ui = :select
     config.columns[:lesson_type].form_ui = :select
     config.columns[:teaching_place].form_ui = :select
