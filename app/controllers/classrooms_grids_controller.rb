c7166886a3e035e6f75140dbd00c71bcf903616f
Andrey Voronkov <andrey@linux-2n20.site>
2010-04-05 09:06:50 +1000

    Grid resource destroyed

--- app/controllers/classrooms_grids_controller.rb
+++ app/controllers/classrooms_grids_controller.rb
@@ -1,4 +1,27 @@
-class ClassroomsGridsController < GridsController
+class ClassroomsGridsController < ApplicationController
+  cattr_reader :days, :times, :weeks
+  @@days = [
+    'Понедельник',
+    'Вторник',
+    'Среда',
+    'Четверг',
+    'Пятница',
+    'Суббота'
+  ]
+  @@times = [
+    '08:15 - 09:45',
+    '09:55 - 11:25',
+    '11:35 - 13:05',
+    '14:00 - 15:30',
+    '15:40 - 17:10',
+    '17:20 - 18:50',
+    '19:00 - 20:30'
+  ]
+  @@weeks = [
+    '1 неделя',
+    '2 неделя'
+  ]
+
 # RESTful methods
   def index
     cookies[:classrooms_grids] = YAML.dump([0])
--- test/unit/grid_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class GridTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
