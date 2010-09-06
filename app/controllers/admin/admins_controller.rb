c017fb398412572c7bfb258dd6c4ccb6f2607bfc
Andrey Voronkov <voronkovaa@gmail.com>
2010-09-06 16:12:42 +1000

    dummy columns for test

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -1,3 +1,5 @@
 class Admin::AdminsController < ApplicationController
-  active_scaffold Admin::Admin
+  active_scaffold Admin::Admin do |config|
+    config.columns = [:name, :login, :email, :password, :password_confirmation]
+  end
 end
