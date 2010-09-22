8473d2a70a2930c9545cba21fb55a5e290472f13
Andrey Voronkov <voronkovaa@gmail.com>
2010-09-22 10:30:11 +1000

    link cleared

--- app/controllers/dept_head/teaching_places_controller.rb
+++ app/controllers/dept_head/teaching_places_controller.rb
@@ -2,6 +2,7 @@ class DeptHead::TeachingPlacesController < DeptHead::BaseController
   active_scaffold do |config|
     config.columns = [:lecturer, :position]
     config.columns[:lecturer].form_ui = :record_select
+    config.columns[:lecturer].clear_link
     config.columns[:position].form_ui = :select
     config.columns[:lecturer].sort_by :sql => 'lecturers.name'
     config.search.columns = :lecturer
