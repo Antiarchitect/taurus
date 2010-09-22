5bdf564d5676c33787a10170befc5ad5aa2a5490
Andrey Voronkov <voronkovaa@gmail.com>
2010-09-22 15:54:54 +1000

    done

--- app/controllers/dept_head/charge_cards_controller.rb
+++ app/controllers/dept_head/charge_cards_controller.rb
@@ -1,8 +1,8 @@
 class DeptHead::ChargeCardsController < DeptHead::BaseController
   active_scaffold do |config|
-    config.columns = [:teaching_place, :discipline, :jets, :lesson_type, :hours_quantity, :groups]
-    config.create.columns.exclude :groups
-    config.update.columns.exclude :groups
+    config.columns = [:teaching_place, :discipline, :jets, :lesson_type, :hours_quantity, :hours_per_week, :weeks_quantity, :groups]
+    config.create.columns.exclude :groups, :hours_quantity
+    config.update.columns.exclude :groups, :hours_quantity
     config.list.columns.exclude :jets
     config.show.columns.exclude :jets
     config.columns[:discipline].form_ui = :record_select
