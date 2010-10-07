14a43ac985775266ec5afba02befcaeaa673e05e
Andrey Voronkov <voronkovaa@gmail.com>
2010-10-08 09:37:49 +1000

    some bells and whistles

--- app/models/discipline.rb
+++ app/models/discipline.rb
@@ -2,6 +2,7 @@ class Discipline < ActiveRecord::Base
   belongs_to :department
   has_many :charge_cards
 
-  validates_presence_of :department, :name
+  validates_presence_of :department, :name, :short_name
   validates_uniqueness_of :name, :scope => :department_id
+  validates_length_of :short_name, :maximum => 24
 end
