d37bdcffa6c3897ea4cffeaba187c6169a02400f
Novikov Andrey <envek@envek.name>
2010-10-12 22:30:49 +1000

    Rewritten AJAX request for group list with warnings and spinner. WARNING: working, but not yet completed (not designed and polished)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -69,7 +69,18 @@ jQuery(document).ready(function($){
         $.post('/editor/classrooms/' + $(this).attr('grid_id'), {_method: 'delete'}, null, "script");
         return false;
     });
-    
+
+    function showErrorNotify (text) {
+        $('<div>', {
+                        id: 'error_notify'
+                    }).text(text).appendTo('body')
+                    .position({
+                        my: 'center top',
+                        at: 'center bottom',
+                        of: '.group_name_input'
+                    }).slideDown().delay(10000).slideUp();
+    };
+
     $('#group_name_input, #group_name_input_terminal').focus();
     $('#group_name_input, #group_name_input_terminal').autocomplete({
         minLength: 2,
@@ -77,17 +88,39 @@ jQuery(document).ready(function($){
           return false;
         },
         source: function(request, response) {
-            $.getJSON('/timetable/groups.json', {
-                group: request.term
-            },
-            function(data) {
-                var label_addon = " ← нажать";
-                response($.map( data, function( item ) {
-                  return {
-                    label: item.group.name + label_addon,
-                    value: item.group.id
-                  }
-                }));
+            // Pre-query interface preparations
+            $('<img />', {
+                src: '/images/loading_16.png',
+                alt: '',
+                id: 'loading_spinner'
+            }).appendTo('body').position({
+                my: 'right center',
+                at: 'right center',
+                of: '.group_name_input'
+            });
+            // Querying for group list
+            $.ajax({
+                url: '/timetable/groups.json',
+                data: {group: request.term},
+                dataType: 'json',
+                timeout: 5000,
+                success: function(data, status) {
+                    if (data === null)
+                        showErrorNotify("Произошла ошибка: не удалось загрузить список групп, попробуйте чуть позже.");
+                    var label_addon = " ← нажать";
+                    response($.map( data, function( item ) {
+                        return {
+                            label: item.group.name + label_addon,
+                            value: item.group.id
+                        }
+                    }));
+                },
+                error: function(xhr, status) {
+                    showErrorNotify("Произошла ошибка: не удалось загрузить список групп, попробуйте чуть позже.");
+                },
+                complete: function(xhr, status) {
+                    $('#loading_spinner').remove();
+                }
             });
         },
         select: function(event, ui) {
