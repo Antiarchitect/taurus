16560f7853cef9f0ce57438ecc6ef101b30a6f84
Andrey Voronkov <voronkovaa@gmail.com>
2010-10-15 13:42:39 +1000

    charge cards can be CRUD from the discipline

--- app/controllers/dept_head/charge_cards_controller.rb
+++ app/controllers/dept_head/charge_cards_controller.rb
@@ -1,11 +1,13 @@
 class DeptHead::ChargeCardsController < DeptHead::BaseController
   active_scaffold do |config|
-    config.columns = [:lesson_type, :jets, :discipline, :hours_quantity, :hours_per_week, :weeks_quantity, :groups]
+    config.columns = [:teaching_place, :lesson_type, :jets, :discipline, :hours_quantity, :hours_per_week, :weeks_quantity, :groups]
     config.create.columns.exclude :groups, :hours_quantity
     config.update.columns.exclude :groups, :hours_quantity
     config.list.columns.exclude :jets
     config.columns[:discipline].form_ui = :record_select
-    config.columns[:lesson_type].form_ui = :select    
+    config.columns[:lesson_type].form_ui = :select
+    config.columns[:teaching_place].form_ui = :select
+    config.columns[:teaching_place].clear_link
     config.columns[:discipline].clear_link
     config.columns[:groups].clear_link
   end
