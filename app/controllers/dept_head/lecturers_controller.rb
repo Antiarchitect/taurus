bc52c020b44d9f28a7695a5556e26d8f8ede3964
Andrey Voronkov <voronkovaa@gmail.com>
2010-10-25 13:56:52 +1000

    Added possibility to add lecturers from TeachingPlaces controller in dept_head

--- app/controllers/dept_head/lecturers_controller.rb
+++ app/controllers/dept_head/lecturers_controller.rb
@@ -1,7 +1,7 @@
 class DeptHead::LecturersController < DeptHead::BaseController
   record_select :search_on => :name, :order_by => :name
   active_scaffold do |config|
-    config.actions = [:list]
+    #config.actions = [:list, :create]
     config.columns = [:name]
   end
 end
